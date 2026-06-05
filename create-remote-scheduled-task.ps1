7bd3819f67f947ea7c59ec8278931b82b3de58e0
Mike D <6719318+mikhaild908@users.noreply.github.com>
2026-06-05 15:29:03 -0700

    initial commit

---
+++ create-remote-scheduled-task.ps1
@@ -0,0 +1,54 @@
+param(
+    [Parameter(Mandatory = $false)]
+    [string]$RemoteServer = "REMOTE-SERVER",
+
+    [Parameter(Mandatory = $false)]
+    [string]$TaskPath = "\FOLDER\SUB-FOLDER",
+
+    [Parameter(Mandatory = $false)]
+    [string]$TaskName = "TASK-NAME",
+
+    [Parameter(Mandatory = $false)]
+    [string]$ScriptPathOnRemoteServer = "C:\applications\app.exe",
+
+    [Parameter(Mandatory = $false)]
+    [string]$ActionArguments = "",
+
+    [Parameter(Mandatory = $false)]
+    [string]$TaskUser = "DOMAIN\USER" # "SYSTEM or USER-GMSA$"
+)
+
+$credential = Get-Credential -UserName "USERNAME" -Message "Enter credentials for $RemoteServer"
+$cimSession = New-CimSession -ComputerName $RemoteServer -Credential $credential
+
+try {
+    # define action (what runs)
+    $action = ($ActionArguments -eq "") ? (New-ScheduledTaskAction -Execute $ScriptPathOnRemoteServer)
+        : (New-ScheduledTaskAction -Execute $ScriptPathOnRemoteServer -Argument $ActionArguments)
+
+    # define principal/security context
+    if($TaskUser -eq "SYSTEM") {
+        $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
+    }
+    elseif ($TaskUser -like "*$") {
+        # gMSA account, example: DOMAIN\USER-GMSA$
+        $principal = New-ScheduledTaskPrincipal -UserId $TaskUser -LogonType Password
+    }
+    else { 
+        # passwordless user mode (S4U): no password stored, but limited for networ access
+        $principal = New-ScheduledTaskPrincipal -UserId $TaskUser -LogonType S4U
+    }
+
+    #build task object with NO trigger (manual run only)
+    $task = New-ScheduledTask -Action $action -Principal $principal
+
+    # register task remotely (overwrite if exists)
+    Register-ScheduledTask -TaskPath $TaskPath -TaskName $TaskName -InputObject $task -CimSession $cimSession -Force
+
+    Write-Host "Scheduled task '$TaskName' created on $RemoteServer (manual run only)." -ForegroundColor Green
+}
+finally {
+    if($cimSession) {
+        Remove-CimSession $cimSession
+    }
+}
