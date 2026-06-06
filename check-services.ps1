dc02ab7bfd182062b9ef6863a6b40cd453f07038
Mike Dumlao <6719318+mikhaild908@users.noreply.github.com>
2026-06-05 22:34:16 -0700

    Update check-services.ps1

--- check-services.ps1
+++ check-services.ps1
@@ -1,7 +1,7 @@
 param([string]$server = "SERVER-NAME")
 $credentials = Get-Credential -UserName USER-NAME
 
-Write-Host "Getting services for StackVision Server [$server]`n" -ForegroundColor Green
+Write-Host "Getting services for Server [$server]`n" -ForegroundColor Green
 
 Invoke-Command -ComputerName $server -Credential $credentials -ScriptBlock {
     Get-Service | Where-Object {
